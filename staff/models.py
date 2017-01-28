aafb8524c26c87a4c66507a833b3651a0da3c601
prasid444 <prasidkarki95@live.com>
2017-01-28 18:56:49 +0545

    trieed model

--- staff/models.py
+++ staff/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Transaction(models.Model):
+
+      date= models.DateTimeField()
+
+      pid= models.CharField(max_length=20)
+      userid = models.CharField(max_length=20)
+        issuedby= models.CharField(max_length=100)
